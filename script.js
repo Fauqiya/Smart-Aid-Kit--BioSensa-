8bae5a5546dfc8f6c051a8e82fe50eca769328d9
Fauqiya Sadaf Firoz Shaikh <fauqiyasadaf710@gmail.com>
2025-10-24 21:10:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Demo form alert
+document.getElementById('demoForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert('Demo request submitted! Thank you for contacting BioSensa.');
+});
